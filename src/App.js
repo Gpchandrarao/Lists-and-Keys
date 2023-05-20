99b2b74e931f53891e185eb7b9209dca2d6cc364
gpchandrarao <gandikotapoornachandrarao@gmail.com>
2023-05-20 05:07:22 +0000

    Lists and Keys

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import MultilingualGreetings from './component/MultilingualGreetings'
+
+import './App.css'
+
+// Replace your code here
+const App = () => <MultilingualGreetings />
+
+export default App
